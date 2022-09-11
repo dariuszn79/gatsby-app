ba6df41450b7beeaf90ce089f57dc88348fe83cc
Dariusz Nowacki <dariusz.nowacki@sky.uk>
2022-09-11 17:16:09 +0100

    first commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -142,7 +142,7 @@ const IndexPage = () => {
       <h1 style={headingStyles}>
         Congratulations
         <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
+        <span style={headingAccentStyles}>— you just made a Gatsby site!</span>
       </h1>
       <p style={paragraphStyles}>
         Edit <code style={codeStyles}>src/pages/index.tsx</code> to see this page
